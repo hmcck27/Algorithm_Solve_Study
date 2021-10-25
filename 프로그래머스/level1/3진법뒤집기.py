c85ece29bcb23478710edf5647ac0de6b651391e
backend <backend@JK.local>
2021-10-25 13:24:42 +0900

    2021/10/24 jk

--- 프로그래머스/level1/3진법뒤집기.py
+++ 프로그래머스/level1/3진법뒤집기.py
@@ -1,5 +1,29 @@
 def solution(n):
     answer = 0
 
+    result = []
+
+    while True:
+        if n/3 < 3:
+            result.append(n%3)
+            if n//3 != 0:
+                result.append(n//3)
+            break
+
+        result.append(n%3)
+        n = n//3
+
+    print(result)
+
+    while result[0] == 0:
+        result.pop(0)
+
+    for i in range(len(result)):
+        answer += result[i] * (3**(len(result)-1-i))
+
 
     return answer
+
+
+n = 1
+print(solution(n))
