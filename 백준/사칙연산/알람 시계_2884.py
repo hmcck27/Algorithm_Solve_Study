afc4e61914e9ae739ce091a7e94a685f08c987af
baekkom180 <baekkom180@naver.com>
2021-10-26 16:53:35 +0900

    2021-10-26 sw(1)

--- 백준/사칙연산/알람 시계_2884.py
+++ 백준/사칙연산/알람 시계_2884.py
@@ -21,7 +21,25 @@
 첫째 줄에 상근이가 창영이의 방법을 사용할 때, 설정해야 하는 알람 시간을 출력한다. (입력과 같은 형태로 출력하면 된다.)
 '''
 
-g
+H, M = map(int, input().split())
+
+
+if H == 0:
+    if M < 45:
+        H = 23
+        M+=15
+        print(H, M)
+    else:
+        M = M-45
+        print(H, M)
+else:
+    if M < 45:
+        H = H - 1
+        M+=15
+        print(H, M)
+    else:
+        M = M-45
+        print(H, M)
 
 
 
